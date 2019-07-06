e4fdc4e475cca6ae6a0210599943a73b8ce47798
BankaNikitha <51703559+BankaNikitha@users.noreply.github.com>
2019-07-06 12:33:48 +0530

    Add files via upload

---
+++ src/main/java/com/apec/navapp/App.java
@@ -0,0 +1,13 @@
+package com.apec.navapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
